d7e4655798fc028a7f429a309d38903b579ace30
Donjet Dana <168549398+DonjetDana@users.noreply.github.com>
2024-06-04 22:45:01 +0200

    Delete orders directory

--- orders/events/publishers/order-cancelled-publisher.ts
+++
@@ -1,5 +0,0 @@
-import { Subjects, Publisher, OrderCancelledEvent } from '@lab2ubt/common';
-
-export class OrderCancelledPublisher extends Publisher<OrderCancelledEvent> {
-  subject: Subjects.OrderCancelled = Subjects.OrderCancelled;
-}
--- orders/events/publishers/order-created-publisher.ts
+++
@@ -1,5 +0,0 @@
-import { Publisher, OrderCreatedEvent, Subjects } from '@lab2ubt/common';
-
-export class OrderCreatedPublisher extends Publisher<OrderCreatedEvent> {
-  subject: Subjects.OrderCreated = Subjects.OrderCreated;
-}
