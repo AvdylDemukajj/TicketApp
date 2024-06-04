721c76ca9c3868d92c5f1b1012ded48d897390e5
kosovareb <avdyldemukaj38@gmail.com>
2024-06-04 11:16:31 +0200

    "start listening to server now after kubernetes config"

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -9,9 +9,9 @@ const start = async () => {
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY must be defined');
   }
-  if (!process.env.MONGO_URI) {
-    throw new Error('MONGO_URI must be defined');
-  }
+  // if (!process.env.MONGO_URI) {
+  //   throw new Error('MONGO_URI must be defined');
+  // }
   // if (!process.env.NATS_CLIENT_ID) {
   //   throw new Error('NATS_CLIENT_ID must be defined');
   // }
@@ -36,8 +36,8 @@ const start = async () => {
     // process.on('SIGTERM', () => natsWrapper.client.close());
     // new OrderCreatedListener(natsWrapper.client).listen();
     // new OrderCancelledListener(natsWrapper.client).listen();
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log('Connected to MongoDb');
+    // await mongoose.connect(process.env.MONGO_URI);
+    // console.log('Connected to MongoDb');
   } catch (err) {
     console.error(err);
   }
