f58f843538c7923f5838d1e02a54aef9e7fd0ac0
kosovareb <avdyldemukaj38@gmail.com>
2024-06-05 16:59:53 +0200

    "adding publishers to the server "

--- payments/src/index.ts
+++ payments/src/index.ts
@@ -2,8 +2,8 @@ import mongoose from 'mongoose';
 
 import { app } from './app';
 import { natsWrapper } from './nats-wrapper';
-// import { OrderCancelledListener } from './events/listeners/order-canceled-listener';
-// import { OrderCreatedListener } from './events/listeners/order-created-listener';
+import { OrderCancelledListener } from './events/listeners/order-canceled-listener';
+import { OrderCreatedListener } from './events/listeners/order-created-listener';
 
 const start = async () => {
   if (!process.env.JWT_KEY) {
@@ -36,8 +36,8 @@ const start = async () => {
     process.on('SIGTERM', () => natsWrapper.client.close());
     await mongoose.connect(process.env.MONGO_URI);
     console.log('Connected to MongoDb');
-    // new OrderCreatedListener(natsWrapper.client).listen();
-    // new OrderCancelledListener(natsWrapper.client).listen();
+    new OrderCreatedListener(natsWrapper.client).listen();
+    new OrderCancelledListener(natsWrapper.client).listen();
   } catch (err) {
     console.error(err);
   }
